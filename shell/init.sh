7bf7a168a6d2bf744b566ed5f5b091c6831b2111
daniel.martin <daniel.martin@mango.com>
2026-01-22 20:09:39 +0100

    feat(init.sh): update PATH exports to include HOME and scripts directories

--- shell/init.sh
+++ shell/init.sh
@@ -2,7 +2,9 @@ source "$DOTFILES_PATH/shell/aliases.sh"
 source "$DOTFILES_PATH/shell/exports.sh"
 source "$DOTFILES_PATH/shell/functions.sh"
 
+export PATH="$PATH:$HOME/bin"
 export PATH="$PATH:$DOTFILES_PATH/bin"
+export PATH="$PATH:$DOTFILES_PATH/scripts"
 
 # Mango
 if [ -d "$DOTFILES_PATH/scripts/.no-commit/mango" ]; then
